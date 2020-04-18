d63fe809c274e5b2922bce8774eda0e0dbf93a31
Anton Kozmodemyanov <antoxa.kozmod@gmail.com>
2020-04-18 09:08:38 +0300

    converted the code using the function

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 var main = function () {
 	"use strict";
-	$(".tabs a:nth-child(1)").on("click", function() {
-		// делаем все вкладки неактивными
+	var makeTabActive = function (tabNumber) {
+		// сконструируем селектор из tabNumber
+		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
 		$(".tabs span").removeClass("active");
-		// делаем активной первую вкладку
-		$(".tabs a:nth-child(1) span").addClass("active");
-		// очищаем основное содержание, чтобы переопределить его
-		$("main.content").empty();
-		// возвращается false, так как мы не переходим по ссылке return false;
+		$(tabSelector).addClass("active");
+	};
+	$(".tabs a:nth-child(1)").on("click", function () {
+		makeTabActive(1);
 		return false;
 	});
 	$(".tabs a:nth-child(2)").on("click", function() {
-		$(".tabs span").removeClass("active");
-		$(".tabs a:nth-child(2) span").addClass("active");
-		$("main .content").empty();
+		makeTabActive(2);
 		return false;
-	})
+	});
 	$(".tabs a:nth-child(3)").on("click", function() {
-		$(".tabs span").removeClass("active");
-		$(".tabs a:nth-child(3) span").addClass("active");
-		$("main .content").empty();
+		makeTabActive(3);
 		return false;
-	})
+	});
 };
-$(document).ready(main);
