bc56fd5fbded4156bb71e10c6706ed2dee089cc6
Anton Kozmodemyanov <antoxa.kozmod@gmail.com>
2020-04-18 10:58:40 +0300

    configuring content

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var main = function () {
 	"use strict";
+	var toDos = [
+		"Закончить писать эту книгу",
+		"Вывести Грейси на прогулку в парк",
+		"Ответить на электронные письма",
+		"Подготовиться к лекции в понедельник",
+		"Обновить несколько новых задач",
+		"Купить продукты"
+	];
 	$(".tabs a span").toArray().forEach(function (element) {
 		// создаем обработчик щелчков для этого элемента
 		$(element).on("click", function() {
@@ -11,14 +19,31 @@ var main = function () {
 			$element.addClass("active");
 			$("main .content").empty();
 			if ($element.parent().is(":nth-child(1)")) {
-				console.log("Щелчок на первой вкладке!");
+				for (var i = toDos.length-1; i > -1; i--) {
+					$(".content").append($("<li>").text(toDos[i]));
+				}
 			} else if ($element.parent().is(":nth-child(2)")) {
-				console.log("Щелчок на второй вкладке!");
+				toDos.forEach(function (todo) {
+					$(".content").append($("<li>").text(todo));
+				});
 			} else if ($element.parent().is(":nth-child(3)")) {
-				console.log("Щелчок на третьей вкладке!");
+				$(".content").append(
+				'<input type="text" class="inp">'+
+				'<button class="btn">Добавить</button>'
+			);
+				var newToDo;
+				$('.btn').on('click',function(){
+					newToDo= $('.inp').val();
+					if (newToDo!='') {
+						toDos.push( newToDo);
+						alert('Новое задание "'+newToDo+'" успешно добавлено!');
+						$('.inp').val("");
+					}
+				});
 			}
 			return false;
 		});
 	});
+	$(".tabs a:first-child span").trigger("click");
 };
 $(document).ready(main);
