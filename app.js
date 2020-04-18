3c8a603d6425b5642bccc696439022c209109559
Anton Kozmodemyanov <antoxa.kozmod@gmail.com>
2020-04-18 07:38:09 +0300

    tabs work

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 var main = function () {
 	"use strict";
-	console.log("hello, world!");
+	$(".tabs a:nth-child(1)").on("click", function() {
+		// делаем все вкладки неактивными
+		$(".tabs span").removeClass("active");
+		// делаем активной первую вкладку
+		$(".tabs a:nth-child(1) span").addClass("active");
+		// очищаем основное содержание, чтобы переопределить его
+		$("main.content").empty();
+		// возвращается false, так как мы не переходим по ссылке return false;
+		return false;
+	});
+	$(".tabs a:nth-child(2)").on("click", function() {
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(2) span").addClass("active");
+		$("main .content").empty();
+		return false;
+	})
+	$(".tabs a:nth-child(3)").on("click", function() {
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(3) span").addClass("active");
+		$("main .content").empty();
+		return false;
+	})
 };
 $(document).ready(main);
