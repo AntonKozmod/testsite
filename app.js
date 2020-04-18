24540eb92c633090f4ffe4421800a27151ba154f
Anton Kozmodemyanov <antoxa.kozmod@gmail.com>
2020-04-18 10:03:10 +0300

    content control

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 var main = function () {
 	"use strict";
-	var tabNumber;
-	for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
-		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-		$(tabSelector).on("click", function () {
-			$(".tabs span").removeClass("active");
-			$(this).addClass("active");
+	$(".tabs a span").toArray().forEach(function (element) {
+		// создаем обработчик щелчков для этого элемента
+		$(element).on("click", function() {
+			// поскольку мы используем версию элемента jQuery,
+			// нужно создать временную переменную, 
+			// чтобы избежать постоянного обновления
+			var $element = $(element);
+			$(".tabs a span").removeClass("active");
+			$element.addClass("active");
+			$("main .content").empty();
+			if ($element.parent().is(":nth-child(1)")) {
+				console.log("Щелчок на первой вкладке!");
+			} else if ($element.parent().is(":nth-child(2)")) {
+				console.log("Щелчок на второй вкладке!");
+			} else if ($element.parent().is(":nth-child(3)")) {
+				console.log("Щелчок на третьей вкладке!");
+			}
 			return false;
 		});
-	};
+	});
 };
 $(document).ready(main);
